5fe601f1e1d1640c476b9cd1da58e5b3c87d0d77
codercanfly <hao.qiao7@hotmail.com>
2017-05-17 09:45:09 -0400

    quick update

--- src/components/signin/signin.component.ts
+++ src/components/signin/signin.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 
+import { ApiService } from '../../services/api/api.service';
+
 @Component({
     selector: 'app-signin',
     templateUrl: 'signin.component.html',
-    styleUrls: ['signin.component.css']
+    styleUrls: ['signin.component.css'],
+    providers: []
 })
 export class SigninComponent {
+    constructor(private api: ApiService) { }
     model = {
         email: null,
         password: null
     }
     login() {
-
+        
     }
 }
